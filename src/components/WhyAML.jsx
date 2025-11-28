75a06c89c61d143cbd49474d039263c13d1966a9
dhanalakshmi sherugar <dhanalakshmisherugar14@gmail.com>
2025-11-28 12:27:22 +0530

    added blue

--- src/components/WhyAML.jsx
+++ src/components/WhyAML.jsx
@@ -22,22 +22,23 @@ const WhyAML = () => {
       <div className="max-w-6xl mx-auto px-6">
 
         {/* Top Heading */}
-        <div className="text-center max-w-4xl mx-auto space-y-6 mb-8"> 
-          <h2 className="text-3xl md:text-4xl font-bold text-slate-900 leading-tight">
+        <div className="text-center max-w-6xl mx-auto space-y-6 mb-8"> 
+          <h2 className="text-4xl md:text-5xl font-bold text-slate-900 leading-tight">
             Why AML Screening Matters
           </h2>
 
           <p className="text-lg md:text-xl text-slate-600 leading-relaxed">
-            Compliance gaps can trigger severe penalties, frozen accounts, and long-term reputational loss.  
-            A single oversight can disrupt operations and expose your fund to multi-layered regulatory risks
-          </p>
-
-          <p className="text-lg md:text-xl text-slate-700 font-medium leading-relaxed">
-            Taghash delivers automated investor verification, risk analysis and ongoing monitoring —  
-            fully aligned with{" "}
-            <span className="text-black font-bold break-words">
+            Compliance gaps can trigger severe penalties, frozen accounts and long-term reputational loss.
+            <br />
+            <br />
+            A single oversight can disrupt operations and expose your fund to multi-layered regulatory risks.
+            <br />
+            <br />
+            Taghash provides investor verification, risk analysis and ongoing monitoring, all aligned with{" "}
+            <span className="text-black font-bold break-words"> <br/>
               SEBI Circular No. SEBI/HO/MIRSD/MIRSDSECFATF/P/CIR/2024/78
             </span>
+            .
           </p>
         </div>
 
@@ -57,7 +58,7 @@ const WhyAML = () => {
                 </div>
               </div>
 
-              <h3 className="text-lg font-bold text-slate-900">
+              <h3 className="text-xl font-bold text-slate-900">
                 {item.title}
               </h3>
             </div>
@@ -67,7 +68,7 @@ const WhyAML = () => {
         {/* CTA */}
         <div className="text-center mt-20">
           <button
-            className="px-12 py-4 rounded-xl text-lg font-semibold 
+            className="px-12 py-4 rounded-xl text-xl font-semibold 
             bg-gradient-to-r from-blue-600 to-blue-700 text-white 
             shadow-lg hover:shadow-blue-700/40 hover:-translate-y-1 
             transition-all duration-300"
